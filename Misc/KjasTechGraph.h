f374fbc4e67b91e2c7c813abfbcb721743428b6b
Dreasconse <42320715+Dreasconse@users.noreply.github.com>
2019-03-01 10:50:38 -0600

    Kjas Tech Tree

--- Misc/KjasTechGraph.h
+++ Misc/KjasTechGraph.h
@@ -1,38 +1,83 @@
 digraph "Base"
 {
 	ranksep=equally;
-	ratio= 0.44;
+	ratio= 0.4;
 	rankdir = "BT";
-	node [shape=box];
-	"Industrial Understanding" -> "Terraforming";
+	node [shape=box width=3 height=2 labelloc=b];
+	{
+		node [color = green];
+		"Microscale Manufacturing" [label="Microscale\nManufacturing\n10"];
+		"Cultural Understanding" [label="Cultural\nUnderstanding\n9"];
+		"Agricultural Understanding" [label="Agricultural\nUnderstanding\n8"];
+		"Genetic Modification" [label="Genetic\nModification\n12"];
+	}
+	"Hyperspace Mining" [label="Hyperspace\nMining\n4"];
+	"Universal Translator" [label="Universal\nTranslator\n6"];
+	"Information Pool" [label="Informaton Pool\nUnderstanding\n11"];
+	"Industrial Understanding" [label="Industrial\nUnderstanding\n8"];
+	Terraforming [shape=Msquare label="Terraforming\n\nYou may colonize lone ice\nworlds. This costs the Kt'zr'kt'rtl\nCost +3.\nEvery 3 ice worlds are\nworth one point\n\n35"];
+	"Genetic Engineering" [label="Genetic\nEngineering\n8"];
+	"Cultural Unification" [label="Cultural\nUnification\n5"];
+	Nanotechnology [label="Nanotechnology\n6"];
+	"Hyperspace Understanding" [label="Hyperspace\nUnderstanding\n4"];
+	
+	"Industrial Understanding" -> Terraforming;
 	"Agricultural Understanding" -> Terraforming;
 	"Genetic Modification" -> "Genetic Engineering";
 	"Genetic Engineering" -> Terraforming;
 	"Cultural Understanding" -> "Cultural Unification";
 	"Microscale Manufacturing" -> Nanotechnology;
 	"Hyperspace Understanding" -> Nanotechnology;
-	"Hyperspace Mining";
-	"Universal Translator";
-	"Information Pool";
 	
-	"Advanced Hyperdrives" [shape=Msquare];
-
-	node [shape=hexagon];
+	
+	node [shape=hexagon width=.5 height=.5 labelloc=b];
+	"Advanced Hyperdrives" [shape=doubleoctagon label="Advanced\nHyperdrives\n4"];
+	
 	{
 		rank = same;
-		Hyperdrive;
-		Escort;
-		"Colony Ship";
+		node [color = green,style=filled];
+		Hyperdrive [label="Light\nHyperdrive\n0"];
+		Escort [label="Escort\n0"];
+		"Colony Ship" [label="Colony Ship\n0"];
 	}
 	
 	{
 		rank = same;
-		Command;
-		"Bulk Hyperdrive";
-		"Battlestation";
-		Shipyard;
-		Diplomats;
+		Command [color = green label="Command\n10"];
+		"Bulk Hyperdrive" [label="Bulk\nHyperdrive\n13"];
+		"Battlestation" [color = green label="Battlestation\n23"];
+		Shipyard [color = green label="Shipyard\n5"];
+		Diplomats [color = green label="Diplomats\n6"];
 	}
+	"Hyperdrive 2" [label="Advanced\nLight Hyperdrive\n15"];
+	"Escort 2" [label="Advanced\nEscort\n9"];
+	"Colony Ship 2" [label="Advanced\nColony Ship\n9"];
+	"Command 2" [label="Advanced\nCommand\n4"];
+	"Bulk Hyperdrive 2" [label="Advanced Bulk\nHyperdrive\n16"];
+	"Battlestation 2" [label="Advanced\nBattlestation\n25"];
+	"Mobile Shipyard" [label="Mobile\nShipyard\n15"];
+	"Diplomats 2" [label="Advanced\nDiplomats\n5"];
+	"Drone Carrier" [label="Drone\nCarrier\n7"];
+	"Drone Carrier 2" [label="Advanced\nDrone Carrier\n5"];
+	"Interdictor" [label="Interdictor\n4"];
+	"Interdictor 2"[label="Advanced\nInterdictor\n4"];
+	Cruiser [label="Cruiser\n7"];
+	"Cruiser 2" [label="Advanced\nCruiser\n9"];
+	"Armored Hyperdrive" [label="Armored\nHyperdrive\n11"];
+	"Armored Hyperdrive 2" [label="Advanced\nArmored Hyperdrive\n11"];
+	Turtle [label="Turtle\n6"];
+	"Turtle 2" [label="Advanced\nTurtle\n15"];
+	Dreadnought [label="Dreadnought\n25"];
+	"Dreadnought 2" [label="Advanced\nDreadnought\n12"];
+	Lancer [label="Lancer\n7"];
+	"Lancer 2" [label="Advanced\nLancer\n4"];
+	Battleship [label="Battleship\n10"];
+	"Battleship 2" [label="Advanced\nBattleship\n6"];
+	Destroyer [label="Destroyer\n8"];
+	"Destroyer 2" [label="Advanced\nDestroyer\n6"];
+	"Planetary Shield" [label="Planetary\nShield\n11"];
+	"Research Center" [label="Research\nCenter\n17"];
+	
 	
 	{
 		weight = 2;
@@ -51,11 +96,13 @@ digraph "Base"
 		Lancer -> "Lancer 2";
 		"Colony Ship" -> "Colony Ship 2";
 		Diplomats -> "Diplomats 2";
+		Battleship -> "Battleship 2";
+		Destroyer -> "Destroyer 2";
 	}
 
 	Command -> {"Drone Carrier" Interdictor "Dreadnought 2"};
 	Hyperdrive -> {"Bulk Hyperdrive" "Armored Hyperdrive"};
-	"Bulk Hyperdrive 2" -> "Advanced Hyperdrives";
+	"Bulk Hyperdrive 2" -> "Advanced Hyperdrives" [headlabel="or"];
 	"Hyperdrive 2"-> "Advanced Hyperdrives";
 	"Advanced Hyperdrives" -> {Interdictor "Hyperspace Understanding" "Hyperspace Mining" Cruiser "Armored Hyperdrive 2"};
 	"Armored Hyperdrive 2" ->"Cruiser 2";
@@ -63,8 +110,7 @@ digraph "Base"
 	Turtle -> {"Interdictor 2" "Command 2" "Armored Hyperdrive" "Dreadnought" "Battleship 2" "Diplomats 2"};
 	Escort ->{Lancer Destroyer};
 	Lancer -> {Cruiser Dreadnought "Battlestation 2" Battleship};
-	Destroyer -> {"Battlestation 2" "Destroyer 2"};
-	Battleship -> "Battleship 2";
+	Destroyer -> {"Battlestation 2" Battleship};
 	"Colony Ship 2" -> {"Planetary Shield" "Research Center"};
 	"Planetary Shield" -> "Escort 2";
 	"Research Center" -> {"Universal Translator" "Information Pool" "Cultural Unification" Terraforming};
